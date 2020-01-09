969795fd38331a58cc48147dd52942ea34220e0d
holliemaethomas <holliemaethomas85@gmail.com>
2020-01-09 10:10:49 -0800

    complete

--- code401challenges/src/main/java/code401challenges/ArrayShift.java
+++ code401challenges/src/main/java/code401challenges/ArrayShift.java
@@ -1,5 +1,17 @@
 package code401challenges;
 
 public class ArrayShift {
+  public static int[] shiftArray(int[] shiftyArr, int addedValue) {
+      int lengthOfArray = shiftyArr.length+1;
+      int middleOfArray = lengthOfArray / 2;
+      int[] returnArray = new int[lengthOfArray];
 
+      for (int i = 0; i < lengthOfArray; i++){
+          if (i > middleOfArray) returnArray[i] = shiftyArr[i];
+          else if (i == middleOfArray) returnArray[i] = addedValue;
+          else returnArray[i] = shiftyArr[i -1];
+      }
+       return returnArray;
+  }
 }
+//nich paro helped me with this lab
