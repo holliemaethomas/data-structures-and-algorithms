6ad6b0a3f62a7ccbc8fa4a87fe16bf0b9867fa4b
holliemaethomas <holliemaethomas85@gmail.com>
2020-01-14 13:44:06 -0800

    not done with lab 06

---
+++ code401challenges/src/main/java/code401challenges/linkedList/LinkedList.java
@@ -0,0 +1,79 @@
+package code401challenges.linkedList;
+
+
+public class LinkedList<T> {
+    private Node<T> head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public boolean isEmpty() {
+        return this.head == null;
+    }
+
+    public void setHead(T value){
+        this.head = new Node<>(value, null);
+    }
+
+    public Node<T> getHead() {
+        return this.head;
+    }
+
+    public void prepend(T value) {
+        Node<T> temp = this.head;
+        this.head = new Node<>(value, temp);
+    }
+
+    public boolean includes (T valueToCheck) {
+        Node<T> current = this.head;
+        while (current != null) {
+            if (current.getValue().equals(valueToCheck)) {
+                return true;
+            }
+            current = current.getNext();
+        }
+        return false;
+    }
+
+    public void append(T value) {
+        Node<T> current = this.head;
+        while (current.getNext() != null){
+            current = current.getNext();
+        }
+        Node<T> newNode = new Node<>(value, null);
+        current.setNext(newNode);
+    }
+
+
+    public void insertBefore(T value, T newValue) {
+        if (this.head == null) {
+            return;
+        }
+        Node<T> current = this.head;
+        if (current.getValue().equals(value)) {
+            prepend(newValue);
+            return;
+        }
+        while (current !=  null) {
+            if (current.getNext() == null) {
+                return;
+            }
+            Node<T> newNode = new Node<>(newValue, current.getNext());
+            current.setNext(newNode);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
+//got help from my buddy Nich.
---
+++ code401challenges/src/main/java/code401challenges/linkedList/Node.java
@@ -0,0 +1,39 @@
+package code401challenges.linkedList;
+
+public class Node<T> {
+    private T value;
+    private Node<T> next;
+    private Node<T> previous;
+
+    Node (T value, Node<T> next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    Node(T value, Node<T> next, Node<T> previous) {
+        this.value = value;
+        this.next = next;
+        this.previous = previous;
+    }
+
+    public T getValue() {
+        return this.value;
+    }
+
+  void setNext(Node<T> node) {
+        this.next = node;
+  }
+
+  public Node<T> getNext() {
+        return this.next;
+  }
+
+  void setPrevious(Node<T> node) {
+        this.previous = node;
+  }
+
+  public Node<T> getPrevious() {
+        return this.previous;
+  }
+
+}
