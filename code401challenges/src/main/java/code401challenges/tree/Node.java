f985b011d06c40a65c0bd580a2d8848b25d77744
holliemaethomas <holliemaethomas85@gmail.com>
2020-01-27 21:33:24 -0800

    lab complete

---
+++ code401challenges/src/main/java/code401challenges/tree/Node.java
@@ -0,0 +1,48 @@
+package code401challenges.tree;
+
+
+
+public class Node<T> {
+    public T value;
+    public Node<T> left;
+    public Node<T> right;
+
+//    constructors
+    public Node(T value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+
+    public Node(T value, Node<T> left, Node<T> right){
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+//setters
+
+    public void setValue(T value) { this.value = value;}
+
+    public void setLeft(Node<T> node){ this.left = node;}
+
+    public void setRight(Node<T> node) { this.right = node; }
+
+//    getters
+
+    public T getValue() { return this.value;}
+
+    public Node<T> getLeft() {
+        return this.left;
+    }
+
+    public Node<T> getRight(){
+        return this.right;
+    }
+
+
+    }
+
+
+//https://www.baeldung.com/java-binary-tree as a resource to set up my build used generics
+//instead of ints from the example because generics are easier to work with in the long-run
---
+++ code401challenges/src/main/java/code401challenges/tree/Tree.java
@@ -0,0 +1,102 @@
+package code401challenges.tree;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+
+public class Tree<T> {
+    Node<T> root;
+
+    public Tree() {this.root = null;}
+
+    public void setRoot(Node<T> node) {this.root = node;}
+
+    public Node<T> getRoot(){return this.root;}
+
+    public boolean isEmpty(){return this.root == null;}
+
+
+//in order
+    public List<T> traverseInOrder() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return traverseInOrder(this.root, new ArrayList<>());
+    }
+
+    private List<T> traverseInOrder (Node<T> current, List<T> returnedList){
+        if (current != null) {
+            traverseInOrder(current.getLeft(), returnedList);
+            returnedList.add(current.getValue());
+            traverseInOrder(current.getRight(), returnedList);
+        }
+        return returnedList;
+
+    }
+
+//pre-order
+    public List<T> traversePreOrder(Node node) {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return traversePreOrder(this.root, new ArrayList<>());
+    }
+
+       private List<T> traversePreOrder(Node<T> current, List<T> returnedList) {
+        if(current != null){
+            returnedList.add(current.getValue());
+            traversePreOrder(current.getLeft(), returnedList);
+            traversePreOrder(current.getRight(), returnedList);
+        }
+        return returnedList;
+    }
+
+//post-order
+public List<T> traversePostOrder() {
+    if (isEmpty()) {
+        throw new NoSuchElementException();
+    }
+    return traversePostOrder(this.root, new ArrayList<>());
+}
+
+    public List traversePostOrder(Node<T> current, List<T> returnedList) {
+        if (current != null) {
+            traversePostOrder(current.getLeft(), returnedList);
+            traversePostOrder(current.getRight(), returnedList);
+            returnedList.add(current.getValue());
+        }
+        return returnedList;
+    }
+
+
+    //insert node
+
+    public Node insertNode(Node<Integer> current, int value){
+
+        if (current == null){
+            return new Node(value);
+        }
+
+        if (value < current.value){
+            current.left = insertNode(current.left, value);
+
+        } else if (value > current.value) {
+            current.right = insertNode(current.right, value);
+
+        } return current;
+
+    }
+
+    public boolean treeContains (Node<Integer> current, int value) {
+        if (current == null) { return false;}
+        if (current.value == value) {return true;}
+
+        return value < current.value
+                ? treeContains(current.left, value)
+                : treeContains(current.right, value);
+    }
+
+}
+//modeled my methods from this source https://www.baeldung.com/java-binary-tree
