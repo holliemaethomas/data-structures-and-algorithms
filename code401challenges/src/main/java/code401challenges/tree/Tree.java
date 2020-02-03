a5631d2b60e5a765d5429ce42c5b7007cc21add2
holliemaethomas <holliemaethomas85@gmail.com>
2020-02-02 21:13:27 -0800

    added code for breadth first search lab 17

--- code401challenges/src/main/java/code401challenges/tree/Tree.java
+++ code401challenges/src/main/java/code401challenges/tree/Tree.java
@@ -1,6 +1,8 @@
 package code401challenges.tree;
 
 
+import code401challenges.stacksandqueues.Queue;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -9,62 +11,82 @@ import java.util.NoSuchElementException;
 public class Tree<T> {
     Node<T> root;
 
-    public Tree() {this.root = null;}
+    public Tree() {
+        this.root = null;
+    }
 
-    public void setRoot(Node<T> node) {this.root = node;}
+    public void setRoot(Node<T> node) {
+        this.root = node;
+    }
 
-    public Node<T> getRoot(){return this.root;}
+    public Node<T> getRoot() {
+        return this.root;
+    }
 
-    public boolean isEmpty(){return this.root == null;}
+    public boolean isEmpty() {
+        return this.root == null;
+    }
 
 
-//in order
+    //in order
     public List<T> traverseInOrder() {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
-        return traverseInOrder(this.root, new ArrayList<>());
+        return traverseInOrder(this.root,
+                new ArrayList<>());
     }
 
-    private List<T> traverseInOrder (Node<T> current, List<T> returnedList){
+    private List<T> traverseInOrder(Node<T> current,
+                                    List<T> returnedList) {
         if (current != null) {
-            traverseInOrder(current.getLeft(), returnedList);
+            traverseInOrder(current.getLeft(),
+                    returnedList);
             returnedList.add(current.getValue());
-            traverseInOrder(current.getRight(), returnedList);
+            traverseInOrder(current.getRight(),
+                    returnedList);
         }
         return returnedList;
 
     }
 
-//pre-order
+    //pre-order
     public List<T> traversePreOrder(Node node) {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
-        return traversePreOrder(this.root, new ArrayList<>());
+        return traversePreOrder(this.root,
+                new ArrayList<>());
     }
 
-       private List<T> traversePreOrder(Node<T> current, List<T> returnedList) {
-        if(current != null){
+    private List<T> traversePreOrder(Node<T> current,
+                                     List<T> returnedList) {
+        if (current != null) {
             returnedList.add(current.getValue());
-            traversePreOrder(current.getLeft(), returnedList);
-            traversePreOrder(current.getRight(), returnedList);
+            traversePreOrder(current.getLeft(),
+                    returnedList);
+            traversePreOrder(current.getRight(),
+                    returnedList);
         }
         return returnedList;
     }
 
-//post-order
-public List<T> traversePostOrder() {
-    if (isEmpty()) {
-        throw new NoSuchElementException();
+    //post-order
+    public List<T> traversePostOrder() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return traversePostOrder(this.root,
+                new ArrayList<>());
     }
-    return traversePostOrder(this.root, new ArrayList<>());
-}
 
-    public List traversePostOrder(Node<T> current, List<T> returnedList) {
+    public List traversePostOrder(Node<T> current,
+                                  List<T> returnedList) {
         if (current != null) {
-            traversePostOrder(current.getLeft(), returnedList);
-            traversePostOrder(current.getRight(), returnedList);
+            traversePostOrder(current.getLeft(),
+                    returnedList);
+            traversePostOrder(current.getRight(),
+                    returnedList);
             returnedList.add(current.getValue());
         }
         return returnedList;
@@ -73,43 +95,66 @@ public List<T> traversePostOrder() {
 
     //insert node
 
-    public Node insertNode(Node<Integer> current, int value){
+    public Node insertNode(Node<Integer> current,
+                           int value) {
 
-        if (current == null){
+        if (current == null) {
             return new Node(value);
         }
 
-        if (value < current.value){
-            current.left = insertNode(current.left, value);
+        if (value < current.value) {
+            current.left = insertNode(current.left,
+                    value);
 
         } else if (value > current.value) {
-            current.right = insertNode(current.right, value);
+            current.right = insertNode(current.right,
+                    value);
 
-        } return current;
+        }
+        return current;
 
     }
 
-    public boolean treeContains (Node<Integer> current, int value) {
-        if (current == null) { return false;}
-        if (current.value == value) {return true;}
+    public boolean treeContains(Node<Integer> current,
+                                int value) {
+        if (current == null) {
+            return false;
+        }
+        if (current.value == value) {
+            return true;
+        }
 
         return value < current.value
-                ? treeContains(current.left, value)
-                : treeContains(current.right, value);
+                ? treeContains(current.left,
+                value)
+                : treeContains(current.right,
+                value);
     }
 
+    public static <T> List<T> breadthFirst(Tree<T> tree) {
+        if (tree.isEmpty()) {
+            throw new NoSuchElementException();
+        }
 
-//    public Tree fizzbuzz(Tree tree) {
-//        fizzbuzzRecursively(tree.root);
-//        return tree;
-//
-//    }
-//    private void fizzbuzzRecursively(Node me){
-//        String val = me.value;
-//        int intyVal = Integer.parseInt(val);
-//        if (intyVal % 15 == 0)
-//
-//    }
+        List<T> response = new ArrayList<>();
+        Queue<Node<T>> queue = new Queue<>();
+        queue.enqueue(tree.getRoot());
+
+        while (!queue.isEmpty()) {
+            Node<T> current = queue.dequeue();
+            response.add(current.getValue());
+            if (current.getLeft() != null) {
+                queue.enqueue(current.getLeft());
+            }
+            if (current.getRight() != null) {
+                queue.enqueue(current.getRight());
+            }
+
+        }
+        return response;
+
+    }
 
 }
 //modeled my methods from this source https://www.baeldung.com/java-binary-tree
+//Nich Paro helped with breadth first
