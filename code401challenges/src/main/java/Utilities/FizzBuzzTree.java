81cb90413f6c8ffbb124a454d87de6feaa215338
holliemaethomas <holliemaethomas85@gmail.com>
2020-02-02 20:44:05 -0800

    added comment

--- code401challenges/src/main/java/Utilities/FizzBuzzTree.java
+++ code401challenges/src/main/java/Utilities/FizzBuzzTree.java
@@ -1,5 +1,33 @@
 package Utilities;
 
-public class FizzBuzzTree {
+
+import code401challenges.tree.Node;
+import code401challenges.tree.Tree;
+
+public class FizzBuzzTree  extends Tree<Object> {
+    private static void recursion(Node<Object> current) {
+        if (current != null) {
+            recursion(current.getLeft());
+            changer(current);
+            recursion(current.getRight());
+        }
+    }
+
+   private static void changer(Node<Object> current) {
+        if ((Integer) current.getValue() % 15 == 0) {
+            current.setValue("FizzBuzz");
+   } else if ((Integer)current.getValue() % 3 == 0) {
+        current.setValue("Fizz");
+    } else if ((Integer)current.getValue() % 5 == 0) {
+        current.setValue("Buzz");
+    }
+}
+
+public static Tree<Object> fizzBuzzTree(Tree<Object> tree) {
+        recursion(tree.getRoot());
+        return tree;
+}
 
 }
+
+//was at nichs house. he helped me out.
