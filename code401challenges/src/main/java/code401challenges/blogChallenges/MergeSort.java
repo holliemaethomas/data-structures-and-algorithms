d2c7994936c88b801175ae547a71075619dc57c5
holliemaethomas <holliemaethomas85@gmail.com>
2020-02-12 15:48:45 -0800

    added code and test for chal 27

--- code401challenges/src/main/java/code401challenges/blogChallenges/MergeSort.java
+++ code401challenges/src/main/java/code401challenges/blogChallenges/MergeSort.java
@@ -2,5 +2,22 @@ package code401challenges.blogChallenges;
 
 public class MergeSort {
 
-    
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+    }
 }
---
+++ code401challenges/src/test/java/code401challenges/blogChallenges/MergeSortTest.java
@@ -0,0 +1,15 @@
+package code401challenges.blogChallenges;
+
+import static org.junit.Assert.*;
+
+public class MergeSortTest {
+
+    @Test
+    public void positiveTest() {
+        int[] actual = { 5, 1, 6, 2, 3, 4 };
+        int[] expected = { 1, 2, 3, 4, 5, 6 };
+        MergeSort.mergeSort(actual, actual.length);
+        assertArrayEquals(expected, actual);
+    }
+
+}
