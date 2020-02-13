f0f5a71268bc90f6a6fda4d758338d9499893ddb
holliemaethomas <holliemaethomas85@gmail.com>
2020-02-13 08:20:48 -0800

    forgot to site my resource

--- code401challenges/src/test/java/code401challenges/blogChallenges/MergeSortTest.java
+++ code401challenges/src/test/java/code401challenges/blogChallenges/MergeSortTest.java
@@ -1,5 +1,7 @@
 package code401challenges.blogChallenges;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MergeSortTest {
@@ -13,3 +15,4 @@ public class MergeSortTest {
     }
 
 }
+//use this as a resource https://www.baeldung.com/java-merge-sort
