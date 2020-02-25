33576115dd0eb0f37fc402530ade08ed7bb0b075
holliemaethomas <holliemaethomas85@gmail.com>
2020-02-25 11:29:27 -0800

    challenge 32

---
+++ code401challenges/src/main/java/code401challenges/RepeatedWord/RepeatedWord.java
@@ -0,0 +1,32 @@
+package code401challenges.RepeatedWord;
+
+public class RepeatedWord {
+
+
+ public static void getCount(String [] args) {
+     int count = 0;
+     String string = "My little Pony, My little Pony";
+
+     string.toLowerCase();
+
+     String[] words = string.split(" ");
+
+     for(int i = 0; i < words.length; i++) {
+         count = 1;
+         for(int j = i+1; j < words.length; j++) {
+             if(words[i].equals(words[j])) {
+                 count++;
+
+                 words[j] = "0";
+             }
+         }
+
+         if(count > 1 && words[i] != "0")
+             System.out.println(words[i]);
+
+     }
+
+
+ }
+}
+
