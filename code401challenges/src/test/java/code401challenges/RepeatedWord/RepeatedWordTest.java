fd3d813b128d9eff3735a767b365ad132974f313
holliemaethomas <holliemaethomas85@gmail.com>
2020-02-25 11:51:26 -0800

    adding image

---
+++ code401challenges/src/test/java/code401challenges/RepeatedWord/RepeatedWordTest.java
@@ -0,0 +1,17 @@
+package code401challenges.RepeatedWord;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RepeatedWordTest {
+
+    @Test
+    public void test_getCount(){
+        assertEquals(
+                "my",
+                "my"
+        );
+    }
+
+}
