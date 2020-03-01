2567f94fcc9905e59cf324f219b2ab6255cd35f7
holliemaethomas <holliemaethomas85@gmail.com>
2020-03-01 15:18:21 -0800

    test not finished

--- code401challenges/src/main/java/code401challenges/breadthFirst/BreadthFirst.java
+++ code401challenges/src/main/java/code401challenges/breadthFirst/BreadthFirst.java
@@ -8,10 +8,10 @@ public class BreadthFirst {
 
     class Graph {
 
-        private int V;   // No. of vertices
-        private LinkedList<Integer> adj[]; //Adjacency Lists
+        private int V;
+        private LinkedList<Integer> adj[];
 
-        // Constructor
+
         Graph(int v) {
             V = v;
             adj = new LinkedList[v];
@@ -19,32 +19,29 @@ public class BreadthFirst {
                 adj[i] = new LinkedList();
         }
 
-        // Function to add an edge into the graph
+
         void addEdge(int v, int w) {
             adj[v].add(w);
         }
 
-        // prints BFS traversal from a given source s
+
         void BFS(int s) {
-            // Mark all the vertices as not visited(By default
-            // set as false)
+
             boolean visited[] = new boolean[V];
 
-            // Create a queue for BFS
+
             LinkedList<Integer> queue = new LinkedList<Integer>();
 
-            // Mark the current node as visited and enqueue it
+
             visited[s] = true;
             queue.add(s);
 
             while (queue.size() != 0) {
-                // Dequeue a vertex from queue and print it
+
                 s = queue.poll();
                 System.out.print(s + " ");
 
-                // Get all adjacent vertices of the dequeued vertex s
-                // If a adjacent has not been visited, then mark it
-                // visited and enqueue it
+
                 Iterator<Integer> i = adj[s].listIterator();
                 while (i.hasNext()) {
                     int n = i.next();
@@ -59,3 +56,4 @@ public class BreadthFirst {
 
     }
 }
+//https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
